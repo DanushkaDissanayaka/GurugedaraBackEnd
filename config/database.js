fc9834a5b98817c4174819ab7b6fd5521332618e
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-02-04 19:47:25 +0530

    fee error

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const conDetails = {
     host:'localhost',
     user:'root',
-    password:'',
+    password:'Dan@7069',
     database:'webdata'
 } // store mysql database connection details for user database connection use
 module.exports.conDetails = conDetails;
