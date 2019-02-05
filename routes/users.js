6c957749362e04adb1f698787871f4535ae08986
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-02-05 12:59:01 +0530

    change office user msg

--- routes/users.js
+++ routes/users.js
@@ -429,7 +429,7 @@ function user(User, ID) {
         return false;
       }
       else {
-        callback({ success: true, msg: 'User registerd' + userDetails.usetID });
+        callback({ success: true, msg: 'User registerd' + " " +userDetails.usetID });
         return true;
       }
     });
